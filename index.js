f71a58bfbae9a05f195c240dee457986ff65cb62
[Yogesh Karoda] <[karodayogesh@gmail.com]>
2025-10-29 12:15:48 +0530

    Added isAbove18 fn

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const age = 25;
 const Name = "YK";
-const dob = 7/5/2002;
+const dob = 7 / 5 / 2002;
 
-function sayName(){
-  console.log("Hello" + Name)
+function sayName() {
+  console.log("Hello" + Name);
 }
 
 function isAbove(age){
