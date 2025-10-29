3b6f67eded0f5790105b717c61ab057561086dda
[Yogesh Karoda] <[karodayogesh@gmail.com]>
2025-10-29 16:30:45 +0530

    new brach added feat-new

--- index.js
+++ index.js
@@ -14,5 +14,5 @@ function hii(){
 function addNewFeature(){
   console.log("This is the new feature");
   console.log("This is the new feature 1");
-   console.log("This is the new feature b");
+  console.log("This is the new feature b");
 }
